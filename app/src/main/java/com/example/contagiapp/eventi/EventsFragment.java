17c17da4d2e01faca63b61b519cc9cbcf4772cc4
Francesco <francescodestefano3997@gmail.com>
2021-01-16 13:19:56 +0100

    forse funziona eventi

--- app/src/main/java/com/example/contagiapp/eventi/EventsFragment.java
+++ app/src/main/java/com/example/contagiapp/eventi/EventsFragment.java
@@ -8,27 +8,30 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.contagiapp.R;
 
-public class EventsFragment extends AppCompatActivity {
+public class EventsFragment extends Fragment {
 
-    private Button new_event;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_events);
+    public class ciao extends AppCompatActivity {
+        private Button new_event;
 
-        //implementazione OnClick
-        new_event = (Button) findViewById(R.id.nuovo_evento);
-        new_event.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNuovoEvento();
-            }
-        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.fragment_events);
 
-    }
+            //implementazione OnClick
+            new_event = (Button) findViewById(R.id.nuovo_evento);
+            new_event.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openNuovoEvento();
+                }
+            });
 
-    public void openNuovoEvento(){
-        Intent newEvents = new Intent(this, NewEventsFragment.class);
-        startActivity(newEvents);
+        }
+
+        public void openNuovoEvento() {
+            Intent newEvents = new Intent(this, NewEventsFragment.class);
+            startActivity(newEvents);
+        }
     }
 }
