9d3ed4b300682953448d3776ace7fddb392d0b33
Francesco <francescodestefano3997@gmail.com>
2021-01-16 20:30:12 +0100

    Wanna vedete voi se riuscite perchè a me ora dovrebbe andare ma va in conflitto con il coso nello switch del MainActivity e ho perso un giorno ma non sono ancora riuscito ad aggiustarlo

--- app/src/main/java/com/example/contagiapp/eventi/EventsFragment.java
+++ app/src/main/java/com/example/contagiapp/eventi/EventsFragment.java
@@ -2,36 +2,80 @@ package com.example.contagiapp.eventi;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import android.view.View;
-import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.contagiapp.HomeFragment;
 import com.example.contagiapp.R;
 
-public class EventsFragment extends Fragment {
+public class EventsFragment extends FragmentActivity {
 
-    public class ciao extends AppCompatActivity {
-        private Button new_event;
-    
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.fragment_events);
-
-            //implementazione OnClick
-            new_event = (Button) findViewById(R.id.nuovo_evento);
-            new_event.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openNuovoEvento();
-                }
-            });
-
-        }
-
-        public void openNuovoEvento() {
-            Intent newEvents = new Intent(this, NewEventsFragment.class);
-            startActivity(newEvents);
-        }
+    public EventsFragment() {
+        // Required empty public constructor
     }
+
+    private Button new_event;
+
+    //@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view;
+        view = inflater.inflate(R.layout.fragment_events, container, false);
+
+        //new_event = (Button) view.findViewById(R.id.nuovo_evento);
+
+        /*new_event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view = inflater.inflate(R.layout.fragment_events, container, false);
+                //openNuovoEvento();
+            }
+        });*/
+
+        return view;
+        //return inflater.inflate(R.layout.fragment_notify, container, false);
+    }
+
+
+
+
+
+    /*
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_events);
+
+        //implementazione OnClick
+        new_event = (Button) getView.findViewById(R.id.nuovo_evento);
+        new_event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNuovoEvento();
+            }
+        });
+
+    }*/
+//public class ciao extends AppCompatActivity {
+    public void openNuovoEvento(View v) {
+        NewEventsFragment new1 = new NewEventsFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new NewEventsFragment()).commit();
+        //new1.onCreate(nuovoEvento);
+        /*Intent newEvents = new Intent(getActivity(), NewEventsFragment.class);
+        startActivity(newEvents);*/
+        //NewEventsFragment view = new NewEventsFragment();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.container, new EventsFragment()).commit();
+        /*LayoutInflater inflater = null;
+        ViewGroup container = null;
+        View view = inflater.inflate(R.layout.fragment_new_events, container, false);*/
+    }//}
 }
--- app/src/main/java/com/example/contagiapp/eventi/NewEventsFragment.java
+++ app/src/main/java/com/example/contagiapp/eventi/NewEventsFragment.java
@@ -1,19 +1,27 @@
 package com.example.contagiapp.eventi;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
 import com.example.contagiapp.R;
-import com.example.contagiapp.ui.login.LoginViewModel;
 
-public class NewEventsFragment extends AppCompatActivity {
+public class NewEventsFragment extends Fragment {
 
-    private LoginViewModel loginViewModel;
+    //private LoginViewModel loginViewModel;
+    public void NewEventsFragment(View v) {
+        // Required empty public constructor
+    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_new_events);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+
+        return inflater.inflate(R.layout.fragment_new_events, container, false);
+        //setContentView(R.layout.fragment_new_events);
         /*loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
