11fb3f357e49a861c03e0a09c0dee7e7012aff76
francesco99 <franc-esco99@hotmail.it>
2021-01-19 12:50:28 +0100

    aggiustata un po' di grafica, non riesco a collegare il tasto registrati alla home

--- app/src/main/java/com/example/contagiapp/registrazione/SignUpActivity.java
+++ app/src/main/java/com/example/contagiapp/registrazione/SignUpActivity.java
@@ -2,18 +2,36 @@ package com.example.contagiapp.registrazione;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.contagiapp.HomeFragment;
+import com.example.contagiapp.MainActivity;
 import com.example.contagiapp.R;
+import com.example.contagiapp.data.amici.FriendProfile;
 
 public class SignUpActivity extends AppCompatActivity {
 
 
+    private Button registrati;
+  /*  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view;
+        view = inflater.inflate(R.layout.activity_registration, container, false);
+        registrati = view.findViewById(R.id.registrati);
+        registrati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HomeFragment.class);
+                startActivity(intent);
+            }
+        });
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        return view;
     }
+       */
 }
