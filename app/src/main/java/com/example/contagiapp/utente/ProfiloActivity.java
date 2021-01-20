bb223a8def18a6570a3e0b351fb8e84cdb3f6b46
danielebalest <75737468+danielebalest@users.noreply.github.com>
2021-01-20 11:14:50 +0100

    creato profilo utente

---
+++ app/src/main/java/com/example/contagiapp/utente/ProfiloActivity.java
@@ -0,0 +1,15 @@
+package com.example.contagiapp.utente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.contagiapp.R;
+
+public class ProfiloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilo);
+    }
+}
