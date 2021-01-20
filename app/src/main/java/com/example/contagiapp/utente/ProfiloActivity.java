a4adc70b6519719fe047abdd03828c864d03f990
danielebalest <75737468+danielebalest@users.noreply.github.com>
2021-01-20 11:57:59 +0100

    apertura fotocamera alla pressione del pulsante 'inserisci certificato'

--- app/src/main/java/com/example/contagiapp/utente/ProfiloActivity.java
+++ app/src/main/java/com/example/contagiapp/utente/ProfiloActivity.java
@@ -2,14 +2,33 @@ package com.example.contagiapp.utente;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.contagiapp.R;
 
 public class ProfiloActivity extends AppCompatActivity {
+    private static final int PHOTO_REQUEST_CODE = 0;
+    private Button certificato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profilo);
+
+        certificato = (Button) findViewById(R.id.certificato);
+        certificato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(photoIntent, PHOTO_REQUEST_CODE);
+            }
+        });
+
+
+
     }
 }
