8aeda1724a2b8f3c93e847b59f218068e3a4c5fe
danielebalest <75737468+danielebalest@users.noreply.github.com>
2021-01-25 00:41:31 +0100

    creata listView in FriendsFragment

--- app/src/main/java/com/example/contagiapp/data/amici/FriendsFragment.java
+++ app/src/main/java/com/example/contagiapp/data/amici/FriendsFragment.java
@@ -3,15 +3,20 @@ package com.example.contagiapp.data.amici;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.contagiapp.R;
 import com.example.contagiapp.gruppi.GroupSearch;
+
+import java.util.ArrayList;
 
 
 /**
@@ -26,21 +31,41 @@ public class FriendsFragment extends Fragment {
 
 
     private Button visualizza_profilo;
+    ListView listView;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view;
         view = inflater.inflate(R.layout.fragment_friends, container, false);
-        visualizza_profilo = view.findViewById(R.id.Visualizza_profilo);
+        listView = (ListView)view.findViewById(R.id.list_view);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Utente1");
+        arrayList.add("Utente2");
+        arrayList.add("Utente3");
+        arrayList.add("Utente4");
+        arrayList.add("Utente5");
+        arrayList.add("Utente6");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), R.layout.support_simple_spinner_dropdown_item, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+
+        /*visualizza_profilo = view.findViewById(R.id.Visualizza_profilo);
         visualizza_profilo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), FriendProfile.class);
                 startActivity(intent);
+
+
+
             }
-        });
+        });*/
 
       return view;
     }
+
 
 }
