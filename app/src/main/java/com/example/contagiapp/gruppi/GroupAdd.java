8b944e41df3a4fb2b42d24a1137374e6c4cce8a9
danielebalest <75737468+danielebalest@users.noreply.github.com>
2021-01-25 22:55:21 +0100

    agggiunta BottomView alle altre activity che non l'avevano però crasha al click FANCULOO

--- app/src/main/java/com/example/contagiapp/gruppi/GroupAdd.java
+++ app/src/main/java/com/example/contagiapp/gruppi/GroupAdd.java
@@ -1,11 +1,19 @@
 package com.example.contagiapp.gruppi;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.example.contagiapp.HomeFragment;
+import com.example.contagiapp.NotifyFragment;
 import com.example.contagiapp.R;
+import com.example.contagiapp.data.amici.FriendsFragment;
+import com.example.contagiapp.eventi.EventsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class GroupAdd extends AppCompatActivity {
 
@@ -13,5 +21,46 @@ public class GroupAdd extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_group_add);
+
+        //BOTTON NAVIGATION
+        //Inizializza e assegna varibaile
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+
+        //imposta nav_events selezionata
+        bottomNavigationView.setSelectedItemId(R.id.nav_group);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuitem) {
+                Fragment fragment = null;
+                switch (menuitem.getItemId())
+                {
+                    case R.id.nav_group:
+                        fragment = new GroupFragment();
+                        break;
+
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case R.id.nav_notify:
+                        fragment = new NotifyFragment();
+                        break;
+
+                    case R.id.nav_events:
+                        fragment = new EventsFragment();
+                        break;
+
+                    case R.id.nav_friends:
+                        fragment = new FriendsFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                return true;
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/contagiapp/utente/ProfiloActivity.java
+++ app/src/main/java/com/example/contagiapp/utente/ProfiloActivity.java
@@ -1,14 +1,23 @@
 package com.example.contagiapp.utente;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.contagiapp.HomeFragment;
+import com.example.contagiapp.NotifyFragment;
 import com.example.contagiapp.R;
+import com.example.contagiapp.data.amici.FriendsFragment;
+import com.example.contagiapp.eventi.EventsFragment;
+import com.example.contagiapp.gruppi.GroupFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfiloActivity extends AppCompatActivity {
     private static final int PHOTO_REQUEST_CODE = 0;
@@ -28,7 +37,44 @@ public class ProfiloActivity extends AppCompatActivity {
             }
         });
 
+        //Inizializza e assegna varibaile
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
 
+        //imposta nav_events selezionata
+        bottomNavigationView.setSelectedItemId(R.id.nav_events);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuitem) {
+                Fragment fragment = null;
+                switch (menuitem.getItemId())
+                {
+                    case R.id.nav_group:
+                        fragment = new GroupFragment();
+                        break;
+
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case R.id.nav_notify:
+                        fragment = new NotifyFragment();
+                        break;
+
+                    case R.id.nav_events:
+                        fragment = new EventsFragment();
+                        break;
+
+                    case R.id.nav_friends:
+                        fragment = new FriendsFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                return true;
+            }
+        });
 
     }
 }
