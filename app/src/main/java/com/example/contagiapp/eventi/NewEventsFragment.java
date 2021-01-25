4c8e03a97ade27441ae7b5dac55ecb3099cd4368
Francesco <francescodestefano3997@gmail.com>
2021-01-25 13:02:42 +0100

    aggiunta di nuovo evento completata al 90%

--- app/src/main/java/com/example/contagiapp/eventi/NewEventsFragment.java
+++ app/src/main/java/com/example/contagiapp/eventi/NewEventsFragment.java
@@ -3,10 +3,18 @@ package com.example.contagiapp.eventi;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.contagiapp.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewEventsFragment extends AppCompatActivity {
 
@@ -28,7 +36,31 @@ public class NewEventsFragment extends AppCompatActivity {
         });
     }
 
-    private void openMainActivity() {
+        private void openMainActivity() {
+        Map<String, Object> evento = new HashMap<>();
 
+        TextView nome = (TextView) findViewById(R.id.textNameEvent);
+        evento.put("nome", nome.getText().toString());
+
+        TextView numeroP = (TextView) findViewById(R.id.editTextNumber);
+        evento.put("num_partecipanti", numeroP.getText().toString());
+
+        //data
+//TODO inserire i dati dell'utente che crea l'evento
+        TextView descrizione = (TextView) findViewById(R.id.editTextTextMultiLine);
+        evento.put("descrizione", descrizione.getText().toString());
+
+        Spinner nazione = (Spinner) findViewById(R.id.spinnerNazioni);
+        evento.put("nazione", nazione.getSelectedItem().toString());
+
+        TextView citta = (TextView) findViewById(R.id.editCittaResidenza);
+        evento.put("citta", citta.getText().toString());
+
+        db.collection("Eventi").add(evento);
+        Toast.makeText(this, "Evento aggiunto", Toast.LENGTH_SHORT).show();
+
+        //Tornare indietro
+        this.finish();
+        
     }
 }
