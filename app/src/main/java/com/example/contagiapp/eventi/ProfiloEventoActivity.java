919ebcccab7ec37f977920c3532f244cfaf3ab4d
Francesco <francescodestefano3997@gmail.com>
2021-01-27 19:12:53 +0100

    fatte cosine

--- app/src/main/java/com/example/contagiapp/eventi/ProfiloEventoActivity.java
+++ app/src/main/java/com/example/contagiapp/eventi/ProfiloEventoActivity.java
@@ -1,4 +1,16 @@
 package com.example.contagiapp.eventi;
 
-public class ProfiloEventoActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.contagiapp.R;
+
+public class ProfiloEventoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilo_evento);
+    }
 }
