a387e82a60206d73506b06b111c4099c1ffd1273
danielebalest <75737468+danielebalest@users.noreply.github.com>
2021-01-28 16:07:11 +0100

    quasi finiti i controlli realTime

--- app/src/main/java/com/example/contagiapp/registrazione/RegistrationActivity.java
+++ app/src/main/java/com/example/contagiapp/registrazione/RegistrationActivity.java
@@ -47,10 +47,18 @@ public class RegistrationActivity extends AppCompatActivity implements AdapterVi
     private TextInputLayout nomeLayout;
     private TextInputLayout cognomeLayout;
     private TextInputLayout phoneLayout;
+    private TextInputLayout mailLayout;
+    private TextInputLayout dataLayout;
+    private TextInputLayout psw1Layout;
+    private TextInputLayout psw2Layout;
 
     private TextInputEditText nome;
     private TextInputEditText cognome;
     private TextInputEditText phone;
+    private TextInputEditText mail;
+    private EditText data;
+    private TextInputEditText psw1;
+    private TextInputEditText psw2;
 
 
 
@@ -70,11 +78,21 @@ public class RegistrationActivity extends AppCompatActivity implements AdapterVi
 
         nome = (TextInputEditText) findViewById(R.id.editTextName);
         cognome = (TextInputEditText) findViewById(R.id.editTextSurname);
-        phone = (TextInputEditText) findViewById(R.id.editTextPhone) ;
+        phone = (TextInputEditText) findViewById(R.id.editTextPhone);
+        data = (EditText) findViewById(R.id.editTextDataNascita);
+        mail = (TextInputEditText) findViewById(R.id.editTextTextEmailAddress);
+        psw1 = (TextInputEditText) findViewById(R.id.editTextTextPassword) ;
+        psw2 = (TextInputEditText) findViewById(R.id.editTextRepeatPassword);
 
         nomeLayout = (TextInputLayout) findViewById(R.id.textInputNameLayout);
         cognomeLayout = (TextInputLayout) findViewById(R.id.textInputSurnameLayout);
         phoneLayout = (TextInputLayout) findViewById(R.id.textInputPhoneLayout);
+        mailLayout = (TextInputLayout) findViewById(R.id.textInputEmailLayout);
+        dataLayout = (TextInputLayout) findViewById(R.id.textInputBirthLayout);
+        psw1Layout = (TextInputLayout) findViewById(R.id.textInputPasswordLayout);
+        psw2Layout = (TextInputLayout) findViewById(R.id.textInputRepeatPasswordLayout);
+
+
 
         // collegamento button registrati con la mainActivity
         signUpButton = (Button) findViewById(R.id.registrati);
@@ -82,24 +100,40 @@ public class RegistrationActivity extends AppCompatActivity implements AdapterVi
             @Override
             public void onClick(View v) {
 
-
-
-                switch (controlli_TextInput(nome, nomeLayout, cognome, cognomeLayout, phone, phoneLayout)){
+                switch (controlli_TextInput(nome, cognome, mail, data, phone, psw1, psw2)){
 
                     case 1:
                         nomeLayout.setError("Inserisci nome");
+                        Toast.makeText(RegistrationActivity.this, "Inserisci nome", Toast.LENGTH_SHORT).show();
                         cognomeLayout.setError(null);
                         phoneLayout.setError(null);
                         break;
                     case 2:
                         cognomeLayout.setError("Inserisci cognome");
+                        Toast.makeText(RegistrationActivity.this, "Inserisci cognome", Toast.LENGTH_SHORT).show();
                         nomeLayout.setError(null);
                         phoneLayout.setError(null);
                         break;
+                        /*
                     case 3:
+                        dataLayout.setError("Inserisci data di nascita");
+                    */
+                    case 4:
+                        mailLayout.setError("Inserisci mail");
+                        break;
+
+                    case 5:
                         phoneLayout.setError("Inserisci cellulare");
+                        Toast.makeText(RegistrationActivity.this, "Inserisci cellulare", Toast.LENGTH_SHORT).show();
                         nomeLayout.setError(null);
                         cognomeLayout.setError(null);
+                        break;
+
+                    case 6:
+                        psw1Layout.setError("Inserisci password");
+                        break;
+                    case 7:
+                        psw2Layout.setError("Inserisci password");
                         break;
 
                     default:
@@ -113,7 +147,7 @@ public class RegistrationActivity extends AppCompatActivity implements AdapterVi
         });
 
         //Date Picker
-        dataNascita = (TextView) findViewById(R.id.dataNascita);
+        dataNascita = (TextView) findViewById(R.id.editTextDataNascita);
         dataNascita.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -148,7 +182,7 @@ public class RegistrationActivity extends AppCompatActivity implements AdapterVi
         };
     }
 
-    private int controlli_TextInput(TextInputEditText name, TextInputLayout nomeLayout, TextInputEditText surname, TextInputLayout surnameLayout, TextInputEditText phone, TextInputLayout phoneLayout){
+    private int controlli_TextInput(TextInputEditText name, TextInputEditText surname, TextInputEditText mail, EditText birth, TextInputEditText phone, TextInputEditText psw1, TextInputEditText psw2){
 
         if(isEmpty(name) == true){
             return 1;
@@ -157,11 +191,26 @@ public class RegistrationActivity extends AppCompatActivity implements AdapterVi
         if(isEmpty(surname) == true){
             return 2;
         }
-
-        if(isEmpty(phone) == true){
+/*
+        if(isEmpty((TextInputEditText) birth) == true){
             return 3;
+        }*/
+
+        if(isEmpty(mail)  == true){
+            return 4;
         }
 
+
+        if(isEmpty(phone) == true){
+            return 5;
+        }
+
+        if(isEmpty(psw1)  == true){
+            return 6;
+        }
+        if(isEmpty(psw2)  == true){
+            return 7;
+        }
 
         return 0;
     }
@@ -198,7 +247,7 @@ public class RegistrationActivity extends AppCompatActivity implements AdapterVi
         RadioButton radiosex = (RadioButton) findViewById(Idselezionato);
         user.put("genere", radiosex.getText().toString());
 
-        TextView date = (TextView) findViewById(R.id.dataNascita);
+        TextView date = (TextView) findViewById(R.id.editTextDataNascita);
         final String appoggio = date.getText().toString();
 
         Spinner nazione = (Spinner) findViewById(R.id.spinnerNazioni);
@@ -289,29 +338,37 @@ public class RegistrationActivity extends AppCompatActivity implements AdapterVi
         }
         if(!condemail) {
             Toast.makeText(this, "formato email non valido", Toast.LENGTH_SHORT).show();
-            finish();
-            startActivity(getIntent());
+            mailLayout.setError("Formato email non valido");
+            //finish();
+            //startActivity(getIntent());
         }else
+
             if(conddata && condemail)
         {
-            Toast.makeText(this, "bisogna avere almeno 14 anni per iscriversi", Toast.LENGTH_SHORT).show();
-            finish();
-            startActivity(getIntent());
+            Toast.makeText(this, "Bisogna avere almeno 14 anni per iscriversi", Toast.LENGTH_SHORT).show();
+            dataLayout.setError("Bisogna avere almeno 14 anni per iscriversi");
+            //finish();
+            //startActivity(getIntent());
         } else {
             if (!(psw1.equals(psw2))) {
                 Toast.makeText(this, "Le password coincidono", Toast.LENGTH_SHORT).show();
-                finish();
-                startActivity(getIntent());
+                psw1Layout.setError("Le password coincidono");
+                psw2Layout.setError("Le password coincidono");
+                //finish();
+                //startActivity(getIntent());
             } else {
                 if (cond) {
+                    psw1Layout.setError(null);
+                    psw2Layout.setError(null);
                     user1.put("password", psw1);
                     user1.put("mail", email);
                     db.collection("Utenti").add(user1);
-                    addToDb();
+                    openMainActivity();
                 } else {
                     Toast.makeText(this, "Mail già esistente", Toast.LENGTH_SHORT).show();
-                    finish();
-                    startActivity(getIntent());
+                    mailLayout.setError("Mail già esistente");
+                    //finish();
+                    //startActivity(getIntent());
                 }
             }
         }
